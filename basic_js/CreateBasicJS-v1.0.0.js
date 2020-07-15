584f35d8fcbec4d25373a16e7cead4dab7df152e
goldcomputer <68282831+goldcomputer@users.noreply.github.com>
2020-07-16 08:46:50 +0900

    Add files via upload

---
+++ basic_js/CreateBasicJS-v1.0.0.js
@@ -0,0 +1,65 @@
+/* Create-Basic-js v1.0.0 */
+
+// Edit this text
+var h2 = '';
+
+function createH2() {
+  document.createElement("h2");
+  document.getElementsByTagName("h2")[0].innerHTML = h2;
+}
+
+// Edit this text
+var h1 = '';
+
+function createH1() {
+  document.createElement("h1");
+  document.getElementsByTagName("h1")[0].innerHTML = h1;
+}
+
+// Edit this text
+var h4 = '';
+
+function createH4() {
+  document.createElement("h4");
+  document.getElementsByTagName("h4")[0].innerHTML = h4;
+}
+
+// Edit this text
+var h5 = '';
+
+function createH5() {
+  document.createElement("h5");
+  document.getElementsByTagName("h5")[0].innerHTML = h5;
+}
+
+// Edit this text
+var h6 = '';
+
+function createH6() {
+  document.createElement("h6");
+  document.getElementsByTagName("h6")[0].innerHTML = h6;
+}
+
+// Edit this text
+var h3 = '';
+
+function createH3() {
+  document.createElement("h3");
+  document.getElementsByTagName("h3")[0].innerHTML = h3;
+}
+
+// Edit this text
+var p = '';
+
+function createP() {
+  document.createElement("p");
+  document.getElementsByTagName("p")[0].innerHTML = p;
+}
+
+// Edit this text
+var span = '';
+
+function createSpan() {
+  document.createElement("span");
+  document.getElementsByTagName("span")[0].innerHTML = span;
+}
